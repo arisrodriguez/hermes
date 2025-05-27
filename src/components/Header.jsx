e5f2b6d816e62eb5db389ec1061fbe9d6481d0b2
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-05-27 17:21:54 -0500

    Cambios en la Ui en el H1 y en el p del creador, agregamos icono al H1, ajuste más responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+// import { FaRunning } from "react-icons/fa";
 /**
  * Componente Header que representa el encabezado de la aplicación.
  * Contiene el título de la aplicación y un enlace de crédito.
@@ -8,16 +7,28 @@ import React from 'react'
 export const Header = () => {
     return (
         <header className=' bg-blue-700 text-white flex'>
-            <nav className='h-[149px] mx-auto md:flex md:w-[700px] text-center content-center justify-center items-center'>
+            <nav className='mx-auto py-6 md:flex md:w-[700px] text-center content-center justify-center items-center'>
 
                 {/* Sección izquierda con el título de la aplicación */}
                 <div className='flex-1'>
-                    <h1 className='font-bold font-sans text-[40px] md:text-left'>HERMES</h1>
+                    <h1 className="text-4xl font-extrabold tracking-wide flex gap-2 items-center justify-center md:justify-start">
+                        {/* <FaRunning className="text-white w-6 h-6 md:w-7 md:h-7" /> */}
+                        HERMES
+                    </h1>
                 </div>
 
                 {/* Sección derecha con información sobre el creador */}
                 <div className='flex-1'>
-                    <p className='font-semibold font-sans italic text-[20px]'>Running calculator by <a className='hover:text-yellow-400' href="https://www.instagram.com/runwithizzat/" target='_blank' rel='noopener noreferrer'>@runwithizzat</a></p>
+                    <p className='font-medium italic text-lg'>Running calculator by{' '}
+                        <a
+                            href="https://www.instagram.com/runwithizzat/"
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className="relative inline-block ml-1 font-semibold text-white transition-all duration-300 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-yellow-400 hover:after:w-full after:transition-all after:duration-300"
+                        >
+                            @runwithizzat
+                        </a>
+                    </p>
                 </div>
             </nav>
         </header>
